8128f25e7ea47e2ad288bcd2f9228932706a9a86
Benjamin Sieffert <benjamin.sieffert@actyx.io>
2020-07-15 13:53:18 +0200

    doc wip

--- js/pond/src/tagging/typed.ts
+++ js/pond/src/tagging/typed.ts
@@ -5,10 +5,26 @@ const namedSubSpace = (rawTag: string, sub: string): string[] => {
 }
 
 export interface TypedTagUnion<E> {
+  /**
+   * Add an alternative set we may also match. E.g. tag0.or(tag1.and(tag2)).or(tag1.and(tag3)) will match:
+   * Events with tag0; Events with both tag1 and tag2; Events with both tag1 and tag3.
+   *
+   * @param ..
+   * @returns       ..
+   */
   or<E1>(tag: TypedTagIntersection<E1>): TypedTagUnion<E1 | E>
 
+  /**
+   * Convert into an untyped TagQuery. This is for internal use.
+   *
+   * @param ..
+   * @returns       ..
+   */
   raw(): TagUnion
 
+  /**
+   * Aggregated type of the Events which may be returned by the contained tags.
+   */
   readonly _dataType?: E
 
   readonly type: 'typed-union'
@@ -16,14 +32,45 @@ export interface TypedTagUnion<E> {
 
 // Must be interface, otherwise inferred (recursive) type gets very large.
 export interface TypedTagIntersection<E> {
+  /**
+   * Add another tag(s) to this requirement. E.g Tag<FooEvent>('foo').and(Tag<BarEvent>('bar')) will require both 'foo' and 'bar'.
+   *
+   * @param ..
+   * @returns       ..
+   */
   and<E1>(tag: TypedTagIntersection<E1>): TypedTagIntersection<Extract<E1, E>>
 
+  /**
+   * Add an alternative set we may also match. E.g. Tag<FooEvent>('foo').or(Tag<BarEvent>('bar')) will match 
+   * each Event with at least 'foo' or 'bar'. Note that after the first `or` invocation you cannot `and` anymore,
+   * so you have to nest the parts yourself: tag0.or(tag1.and(tag2)).or(tag1.and(tag3)) etc.
+   *
+   * @param ..
+   * @returns       ..
+   */
   or<E1>(tag: TypedTagIntersection<E1>): TypedTagUnion<E1 | E>
 
+
+  /**
+   * The same requirement, but matching only Events emitted by the very node the code is run on.
+   *
+   * @param ..
+   * @returns       ..
+   */
   local(): TypedTagIntersection<E>
 
+
+  /**
+   * Convert into an untyped TagQuery. This is for internal use.
+   *
+   * @param ..
+   * @returns       ..
+   */
   raw(): TagIntersection
 
+  /**
+   * Aggregated type of the Events which may be returned by the contained tags.
+   */
   readonly _dataType?: E
 
   readonly type: 'typed-intersection'
@@ -44,6 +91,9 @@ export const Tag = <E>(rawTag: string): Tag<E> => ({
   ...req(false, [rawTag]),
 })
 
+/**
+ * Typed expression for tag statements.
+ */
 export type Where<E> = TypedTagUnion<E> | TypedTagIntersection<E>
 
 const req = <E>(onlyLocalEvents: boolean, rawTags: string[]): TypedTagIntersection<E> => {
