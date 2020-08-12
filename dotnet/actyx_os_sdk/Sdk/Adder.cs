3c00e17e385c2b396daa8587c4cea4cfd14dcd07
Nikita Moshensky <]=[poikj-0o98u7hy;6lkjhgfdx>
2020-08-12 22:22:26 +0300

    add stylecop

--- dotnet/actyx_os_sdk/Sdk/Adder.cs
+++ dotnet/actyx_os_sdk/Sdk/Adder.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 
-namespace Sdk
-{
-    public class Adder
-    {
-        public static int Add(int a, int b) => a + b;
-    }
+namespace Sdk {
+  public class Adder {
+    public static int Add(int a, int b) => a + b;
+  }
 }
