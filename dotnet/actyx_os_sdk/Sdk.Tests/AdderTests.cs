807a4b9dfe1b7be8a7157fc697506fd73f48381e
Nikita Moshensky <]=[poikj-0o98u7hy;6lkjhgfdx>
2020-08-12 22:01:04 +0300

    scaffold solution

---
+++ dotnet/actyx_os_sdk/Sdk.Tests/AdderTests.cs
@@ -0,0 +1,15 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Sdk.Tests
+{
+    public class AdderTets
+    {
+        [Fact]
+        public void Adder_Should_Add_Numbers()
+        {
+            Adder.Add(1, 3).Should().Equals(4);
+        }
+    }
+}
---
+++ dotnet/actyx_os_sdk/Sdk/Adder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Sdk
+{
+    public class Adder
+    {
+        public static int Add(int a, int b) => a + b;
+    }
+}
