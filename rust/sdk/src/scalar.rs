0361e5f4b3aaffea512171ff15b4222927a5f875
Oliver Wangler <oliver@wngr.de>
2020-09-02 13:30:21 +0000

    add std::fmt::Display for scalar types

--- rust/sdk/src/scalar.rs
+++ rust/sdk/src/scalar.rs
@@ -73,5 +73,11 @@ macro_rules! mk_scalar {
                 self.0.as_ref()
             }
         }
+
+        impl ::std::fmt::Display for $id {
+            fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
     };
 }
