2e6d7fed7b67f35313c9e8b513ca3ef11a4137a5
Roland Kuhn <rk@rkuhn.info>
2020-09-22 10:32:42 +0200

    don’t forget final characters

--- integration/jest/teardown.ts
+++ integration/jest/teardown.ts
@@ -5,30 +5,26 @@ import { MyGlobal } from './setup'
 const teardown = async (_config: Record<string, unknown>): Promise<void> => {
   const nodeSetup = (<MyGlobal>global).nodeSetup
 
+  process.stdout.write('****\n\nSHUTTING DOWN\n\n')
+
+  await new Promise((res, rej) => {
+    Promise.all(
+      nodeSetup.nodes.map((node) =>
+        node.shutdown().catch((err) => {
+          console.log(`node ${node.name} error while shutting down: ${err}`)
+        }),
+      ),
+    ).then(() => res())
+    setTimeout(() => rej(new Error('timeout waiting for shutdown')), 10_000)
+  }).catch(console.error)
+  await deleteKey(nodeSetup.ec2, nodeSetup.key.keyName).catch(console.error)
+
   for (const name in nodeSetup.logs) {
     process.stdout.write(`****\nlogs for node ${name}\n****\n`)
     for (const entry of nodeSetup.logs[name]) {
       process.stdout.write(`${entry.time.toISOString()} ${entry.line}\n`)
     }
   }
-
-  process.stdout.write('****\n\nSHUTTING DOWN\n\n')
-
-  try {
-    await new Promise((res, rej) => {
-      Promise.all(
-        nodeSetup.nodes.map((node) =>
-          node.shutdown().catch((err) => {
-            console.log(`node ${node.name} error while shutting down: ${err}`)
-          }),
-        ),
-      ).then(() => res())
-      setTimeout(() => rej(new Error('timeout waiting for shutdown')), 10_000)
-    })
-  } catch (e) {
-    console.error(e)
-  }
-  await deleteKey(nodeSetup.ec2, nodeSetup.key.keyName)
 }
 
 export default teardown
