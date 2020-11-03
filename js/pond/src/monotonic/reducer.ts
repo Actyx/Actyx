10a1b14fa30d05323de790e713853190f04e82df
Benjamin Sieffert <benjamin.sieffert@actyx.io>
2020-11-03 09:44:59 +0100

    structure, pending scheduler integration

--- js/pond/src/monotonic/reducer.ts
+++ js/pond/src/monotonic/reducer.ts
@@ -1,60 +1,117 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { greaterThan } from 'fp-ts/lib/Ord'
 import { Event, Events, OffsetMap } from '../eventstore/types'
-import { LocalSnapshot, StateWithProvenance } from '../types'
+import { EventKey, LocalSnapshot, StateWithProvenance } from '../types'
+
+export type PendingSnapshot = {
+  snap: LocalSnapshot<string>
+  tag: string
+}
 
 export type Reducer<S> = {
   appendEvents: (
     events: Events,
     emit: boolean,
   ) => {
-    snapshots: LocalSnapshot<string>[]
+    snapshots: PendingSnapshot[]
     emit: StateWithProvenance<S>[]
   }
 
-  setState: (state: StateWithProvenance<string>) => void
+  setState: (state: LocalSnapshot<string>) => void
 }
 
 export const stateWithProvenanceReducer = <S>(
   onEvent: (oldState: S, event: Event) => S,
-  initialState: StateWithProvenance<S>,
+  initialState: LocalSnapshot<S>,
   deserializeState?: (jsonState: unknown) => S,
 ): Reducer<S> => {
   const deserialize = deserializeState
     ? (s: string): S => deserializeState(JSON.parse(s))
     : (s: string): S => JSON.parse(s) as S
 
-  const cloneSwp = (stateWithProvenance: StateWithProvenance<S>) => {
-    const offsets = { ...stateWithProvenance.psnMap }
-    const state = deserialize(JSON.stringify(swp.state))
+  const cloneSnap = (snap: LocalSnapshot<S>) => {
+    const offsets = { ...snap.psnMap }
+    const state = deserialize(JSON.stringify(snap.state))
     return {
+      ...snap,
       psnMap: offsets,
       state,
     }
   }
 
-  let swp = cloneSwp(initialState)
+  let head = cloneSnap(initialState)
+
+  let queue = snapshotQueue()
+
+  const snapshotEligible = (_latest: EventKey) => (_snapBase: EventKey) => false
 
   return {
     appendEvents: (events: Events, emit: boolean) => {
-      let { state, psnMap } = swp
+      let { state, psnMap, eventKey } = head
 
       for (const ev of events) {
         state = onEvent(state, ev)
         psnMap = OffsetMap.update(psnMap, ev)
+        eventKey = ev
       }
 
-      swp = { state, psnMap }
+      head = {
+        state,
+        psnMap,
+        cycle: head.cycle + events.length,
+        eventKey,
+        horizon: head.horizon, // TODO: Detect new horizons from events
+      }
 
       return {
-        snapshots: [],
+        snapshots: queue.getSnapsToStore(snapshotEligible(eventKey)),
         // This is for all downstream consumers, so we clone.
-        emit: emit ? [cloneSwp(swp)] : [],
+        emit: emit ? [cloneSnap(head)] : [],
       }
     },
 
-    setState: s => {
-      const oldState = deserialize(s.state)
+    setState: snap => {
+      if (eventKeyGreater(snap.eventKey, head.eventKey)) {
+        // Time travel to future: Reset queue
+        queue = snapshotQueue()
+      } else {
+        // Time travel to the past: All newer cached states are invalid
+        queue.invalidateLaterThan(snap.eventKey)
+      }
+
+      const oldState = deserialize(snap.state)
       // Clone the input offsets, since they may not be mutable
-      swp = { psnMap: { ...s.psnMap }, state: oldState }
+      head = { ...snap, psnMap: { ...snap.psnMap }, state: oldState }
     },
+  }
+}
+
+const eventKeyGreater = greaterThan(EventKey.ord)
+
+const snapshotQueue = () => {
+  const queue: PendingSnapshot[] = []
+
+  const addPending = (snapshotsAscending: PendingSnapshot[]) => queue.push(...snapshotsAscending)
+
+  const invalidateLaterThan = (cutOff: EventKey) => {
+    while (queue.length > 0 && eventKeyGreater(queue[queue.length - 1].snap.eventKey, cutOff)) {
+      queue.pop()
+    }
+  }
+
+  const getSnapsToStore = (storeNow: (snapshotKey: EventKey) => boolean): PendingSnapshot[] => {
+    const res = []
+
+    while (queue.length > 0 && storeNow(queue[0].snap.eventKey)) {
+      res.push(queue.shift()!)
+    }
+
+    return res
+  }
+
+  return {
+    addPending,
+    invalidateLaterThan,
+    getSnapsToStore,
   }
 }
