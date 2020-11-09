9dbde02ce7b75027c9bb8b5b11b80457cc3dcc4b
GibboK <gibbok.coding@gmail.com>
2020-11-09 14:37:00 +0100

    save

--- integration/src/ax/local-docker/local-docker.ts
+++ integration/src/ax/local-docker/local-docker.ts
@@ -13,18 +13,26 @@ const mkRun = (opts: string[] = []) => (actyxosDataPath: string): string =>
     '-p 4457:4457',
     '-p 127.0.0.1:4243:4243',
     '-p 127.0.0.1:4454:4454',
+    '--privileged',
     ...opts,
     'actyx/os',
   ].join(' ')
 
 // TODO: unit test
-export const runOnLinux = mkRun(['--privileged --network=host'])
+export const runOnLinux = mkRun(['--network=host'])
 // TODO: unit test
 export const runOnWinMac = mkRun()
+
+const tap = <T>(cb: (x: T) => void) => (x: T): T => {
+  cb(x)
+  return x
+}
 
 export const runLocalDocker = async (actyxosDataPath: string): Promise<void> =>
   ['win32', 'darwin', 'linux'].includes(platform())
     ? execa
-        .command((platform() === 'linux' ? runOnLinux : runOnWinMac)(actyxosDataPath))
+        .command(
+          tap(console.log)((platform() === 'linux' ? runOnLinux : runOnWinMac)(actyxosDataPath)),
+        )
         .then(() => undefined)
     : Promise.reject(`Cannot run Docker, platform ${platform()} is not supported!`)
