aec87742ef2df8892690269e6d31352c27624842
GibboK <gibbok.coding@gmail.com>
2020-11-12 12:11:38 +0100

    add waitForStop utility

--- integration/src/jest-util.ts
+++ integration/src/jest-util.ts
@@ -1,2 +1,34 @@
+import { stubNode } from './stubs'
+
 export const waitForMs = (ms: number): Promise<void> =>
   new Promise((res) => setTimeout(() => res(), ms))
+
+export const waitForStop = (checkEveryMs: number, timeoutMs: number) => (
+  appId: string,
+): Promise<string> => {
+  const started = process.hrtime()
+  return new Promise((res, rej) => {
+    const check = () => {
+      const [diffSeconds] = process.hrtime(started)
+      if (diffSeconds >= timeoutMs / 1000) {
+        rej('waitForStop timeout')
+        return
+      }
+      setTimeout(async () => {
+        const resultLs = await stubNode.ax.Apps.Ls()
+        if (resultLs.code === 'OK') {
+          const app = resultLs.result.find((a) => a.appId === appId)
+          const isAppStopped = app?.running === false
+          if (isAppStopped) {
+            res(`${app?.appId} is stopped`)
+            return
+          } else {
+            check()
+          }
+        }
+      }, checkEveryMs)
+    }
+
+    check()
+  })
+}
