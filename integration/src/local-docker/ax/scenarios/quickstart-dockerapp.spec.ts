d95475bae82fa76ab0591909a57260b44337627f
GibboK <gibbok.coding@gmail.com>
2020-11-13 09:17:48 +0100

    remove duplicated code from test, now we are using an utility

--- integration/src/local-docker/ax/scenarios/quickstart-dockerapp.spec.ts
+++ integration/src/local-docker/ax/scenarios/quickstart-dockerapp.spec.ts
@@ -1,62 +1,32 @@
 import { remove } from 'fs-extra'
 import { stubNode } from '../../../stubs'
-import { SettingsInput } from '../../../cli/exec'
 import { quickstartDirs } from '../../../setup-projects/quickstart'
-import { waitForStop } from '../../local-docker-util'
+import { resetTestEviroment, waitForStop } from '../../local-docker-util'
 
 const WAIT_TIMEOUT_MS = 20_000
 const TRY_FREQUENCY_MS = 1_000
+
+const APP_ID = 'com.actyx.sample-docker-app'
+const TARBALL_FILE = 'com.actyx.sample-docker-app-1.0.0-x86_64.tar.gz' // SPO double check this
 
 const waitStop = waitForStop(TRY_FREQUENCY_MS, WAIT_TIMEOUT_MS)
 const waitStopDockerApp = waitStop('com.actyx.sample-docker-app')
 
 describe('quickstart-dockerapp', () => {
-  const scope = 'com.actyx.os'
-  const appId = 'com.actyx.sample-docker-app'
-  const tarballFile = 'com.actyx.sample-docker-app-1.0.0-x86_64.tar.gz' // SPO double check this
-
-  const stopAndUndeployAllApps = async () => {
-    const responseLs = await stubNode.ax.Apps.Ls()
-    if (responseLs.code === 'OK') {
-      const apps = responseLs.result.map((r) => ({
-        appId: r.appId,
-        running: r.running,
-      }))
-      const hasApps = apps.length > 0
-      if (hasApps) {
-        const appsRunning = apps.filter((a) => a.running === true)
-        const hasAppsRunning = appsRunning.length > 0
-        if (hasAppsRunning) {
-          appsRunning.forEach(async (a) => {
-            stubNode.ax.Apps.Stop(a.appId)
-            await waitStop(a.appId)()
-          })
-        }
-        apps.forEach((app) => stubNode.ax.Apps.Undeploy(app.appId))
-      }
-    }
-    expect(responseLs).toMatchCodeOk()
+  const reset = async () => {
+    await remove(`${TARBALL_FILE}`)
+    await resetTestEviroment()
   }
 
-  const resetTestEviroment = async () => {
-    await stubNode.ax.Settings.Unset(scope)
-    await remove(`${tarballFile}`)
-    await stopAndUndeployAllApps()
-  }
+  beforeEach(async () => await reset())
 
-  beforeEach(async () => await resetTestEviroment())
-
-  afterEach(async () => await resetTestEviroment())
+  afterEach(async () => await reset())
 
   test('quickstart/sample-docker-app run deploy/start/ls/stop/undeploy', async () => {
-    await stubNode.ax.Settings.Set(
-      scope,
-      SettingsInput.FromFile(`${quickstartDirs.quickstart}/misc/local-sample-node-settings.yml`),
-    )
     const responseDeploy = await stubNode.ax.Apps.Deploy(quickstartDirs.sampleDockerApp)
     expect(responseDeploy).toMatchCodeOk()
 
-    const responseStart = await stubNode.ax.Apps.Start(appId)
+    const responseStart = await stubNode.ax.Apps.Start(APP_ID)
     expect(responseStart).toMatchCodeOk()
 
     const responseLs1 = await stubNode.ax.Apps.Ls()
@@ -78,12 +48,12 @@ describe('quickstart-dockerapp', () => {
     }
     expect(responseLs1).toMatchObject(responseLs1Shape)
 
-    const responseStop = await stubNode.ax.Apps.Stop(appId)
+    const responseStop = await stubNode.ax.Apps.Stop(APP_ID)
     expect(responseStop).toMatchCodeOk()
 
     await waitStopDockerApp()
 
-    const responseUndeploy = await stubNode.ax.Apps.Undeploy(appId)
+    const responseUndeploy = await stubNode.ax.Apps.Undeploy(APP_ID)
     expect(responseUndeploy).toMatchCodeOk()
 
     const responseLs2 = await stubNode.ax.Apps.Ls()
