ad9a35a17962d5a05524d7f3ad31f2357b623729
Rüdiger Klaehn <rklaehn@protonmail.com>
2021-01-21 08:41:03 +0100

    Export StreamNr so that it can be used from the banyan store

--- rust/sdk/src/tagged/mod.rs
+++ rust/sdk/src/tagged/mod.rs
@@ -33,7 +33,7 @@ mod tags;
 #[cfg(feature = "client")]
 pub use client::EventService;
 pub use event::{Event, EventKey, Metadata};
-pub use scalars::{AppId, NodeId, SessionId, StreamId};
+pub use scalars::{AppId, NodeId, SessionId, StreamId, StreamNr};
 pub use service::{StartFrom, SubscribeMonotonicRequest, SubscribeMonotonicResponse};
 pub use snapshot::{Compression, SnapshotData};
 pub use tags::{Tag, TagSet};
