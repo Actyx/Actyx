94c639662a8d0bd73fee806a40f8ace0726a7190
Rüdiger Klaehn <rklaehn@protonmail.com>
2021-02-24 10:58:37 +0100

    Implement Decode and Encode for AxTrees structs

--- rust/sdk/src/test_util.rs
+++ rust/sdk/src/test_util.rs
@@ -1,7 +1,7 @@
 // parses hex from cbor.me format
 pub fn from_cbor_me(text: &str) -> anyhow::Result<Vec<u8>> {
     let parts = text
-        .split("\n")
+        .split('\n')
         .filter_map(|x| x.split('#').next())
         .flat_map(|x| x.split_whitespace())
         .collect::<String>();
