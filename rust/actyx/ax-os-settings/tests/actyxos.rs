b371b3f1c150a64d14af663d6ea0da199bce5156
Oliver Wangler <oliver@wngr.de>
2021-03-12 08:06:44 +0000

    another path

--- rust/actyx/ax-os-settings/tests/actyxos.rs
+++ rust/actyx/ax-os-settings/tests/actyxos.rs
@@ -5,7 +5,7 @@ use axossettings::{Scope, Validator};
 #[test]
 fn actyx_os_empty() {
     let schema = serde_json::from_reader(
-        std::fs::File::open("../../protocols/json-schema/os/node-settings.schema.json").unwrap(),
+        std::fs::File::open("../../../protocols/json-schema/os/node-settings.schema.json").unwrap(),
     )
     .unwrap();
     Validator::new(schema)
