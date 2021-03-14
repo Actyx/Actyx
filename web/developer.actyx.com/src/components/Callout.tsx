42eab1208dcdfce3cd4d4d32d7c5897dcf4931b5
nicolas-actyx <nicolas@actyx.io>
2021-03-14 08:52:19 +0100

    mw

--- web/developer.actyx.com/src/components/Callout.tsx
+++ web/developer.actyx.com/src/components/Callout.tsx
@@ -5,14 +5,15 @@ import Markdown from 'react-markdown'
 
 const Wrapper = styled.div`
   border-radius: 8px;
-  max-width: 800px;
+  width: 100%;
   min-height: 92px;
-  background: #f5f6f7;
-  border: 1px solid #eef1f5;
-  padding: 18px;
+  background: #f7fafc;
+  border: 1px solid #e3e8ee;
+  padding: 14px;
   display: grid;
   grid-template-columns: [first] 32px [line2] auto [end];
   grid-template-rows: [row1-start] 32px [row1-end] auto [row2-end];
+  font-size: 13px;
 `
 const Icon = styled.div`
   font-size: 20px;
