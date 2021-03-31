3e2c9e03520f195752c71be3729782dbf514df51
mhaushofer <maximilian@actyx.io>
2021-03-31 15:45:16 +0200

    fix links

--- web/developer.actyx.com/src/contents/conceptual-guides.ts
+++ web/developer.actyx.com/src/contents/conceptual-guides.ts
@@ -7,50 +7,50 @@ const createConceptuals = () => {
     {
       title: 'Building Event-based Systems',
       description: 'Fundamental principles that apply when working with event-based systems.',
-      link: '',
+      link: 'event-based-systems',
     },
     {
       title: 'Distributed systems architectures and caveats',
       description:
         'Fundamental principles that apply when working in distributed systems, such as eventual consistency and the CAP theorem.',
-      link: '',
+      link: 'distributed-system-architectures',
     },
     {
       title: 'Local-First Cooperation Paradigm',
       description: 'How the LFC paradigm works and how its principles apply to factory automation.',
-      link: '',
+      link: 'local-first-cooperation',
     },
     {
       title: 'Actyx Jargon',
       description:
         'Actyx has lots of amazing features that make factory automation easier. But, like any technology, that means there is some jargon that can be confusing to newcomers.',
-      link: '',
+      link: 'actyx-jargon',
     },
     {
       title: 'Peer Discovery',
       description:
         'In distributed systems, many nodes need to collaborate closely. How do they find and talk to each other?',
-      link: '',
+      link: 'peer-discovery',
     },
     {
       title: 'Performance and limits of Actyx',
       description: 'Performance limitations in real-world factory solutions.',
-      link: '',
+      link: 'performance-and-limits-of-actyx',
     },
     {
       title: 'Security in Actyx',
       description: 'Basic netowrk security guarantees provided by ActyxOS.',
-      link: '',
+      link: 'security-in-actyx',
     },
     {
       title: 'ActyxOS components',
       description: 'Architecture and components of ActyxOS.',
-      link: '',
+      link: 'the-actyx-node',
     },
     {
       title: 'ActyxOS node and app lifecycle',
       description: 'Lifecycle of ActyxOS and apps deployed to ActyxOS runtimes.',
-      link: '',
+      link: 'actyx-node-lifecycle',
     },
   ]
   return conceptualGuides
