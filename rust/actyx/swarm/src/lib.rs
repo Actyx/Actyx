63ca7677e5d5c4d208c9632f82aac96138081e5e
Rüdiger Klaehn <ruediger@actyx.io>
2021-05-18 11:35:42 +0200

    Get the snapshot only when we know we will need it.
    Co-authored-by: André Rüdiger <andre.ruediger@gmail.com>

--- rust/actyx/swarm/src/lib.rs
+++ rust/actyx/swarm/src/lib.rs
@@ -737,11 +737,11 @@ impl BanyanStore {
         // take a snapshot of the initial state
         let mut guard = stream.transaction();
         let res = f(&txn, &mut guard);
-        let curr = guard.snapshot();
         if res.is_err() {
             // stream builder state will be reverted, except for the cipher offset
             return res;
         }
+        let curr = guard.snapshot();
         if curr.link() == prev.link() {
             // nothing to do, return
             return res;
