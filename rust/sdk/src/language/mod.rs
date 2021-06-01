9fd489fa8a2a4ce63c39e7a68f8023fc6ed001bb
Rüdiger Klaehn <rklaehn@protonmail.com>
2021-06-01 11:49:27 +0200

    Add helper methods to extract enum cases, and use them
    The alternative would be to use a crate like https://docs.rs/enum_extract/0.1.1/enum_extract/

--- rust/sdk/src/language/mod.rs
+++ rust/sdk/src/language/mod.rs
@@ -58,6 +58,23 @@ pub enum TagAtom {
     AppId(AppId),
 }
 
+impl TagAtom {
+    pub fn tag(&self) -> Option<&Tag> {
+        if let Self::Tag(tag) = self {
+            Some(tag)
+        } else {
+            None
+        }
+    }
+    pub fn is_local(&self) -> bool {
+        if let Self::IsLocal = self {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 // this will obviously need to be implemented for real sometime, with arbitrary precision
 #[derive(Debug, Clone)]
 pub enum Number {
