6e0dfb126482bef1521715f0d72fdc9dfd40e462
Oliver Wangler <oliver@wngr.de>
2021-06-08 21:41:07 +0200

    Add test to assert compaction loop runs.
    Closes #5415

--- rust/actyx/swarm/src/tests.rs
+++ rust/actyx/swarm/src/tests.rs
@@ -1,10 +1,10 @@
 use std::{collections::BTreeMap, convert::TryFrom, str::FromStr, time::Duration};
 
 use crate::BanyanStore;
-use actyx_sdk::{Payload, StreamNr, Tag, TagSet};
-use ax_futures_util::stream::interval;
+use actyx_sdk::{tags, Payload, StreamNr, Tag, TagSet};
+use ax_futures_util::{prelude::AxStreamExt, stream::interval};
 use banyan::query::AllQuery;
-use futures::prelude::*;
+use futures::{prelude::*, StreamExt};
 use libipld::Cid;
 
 struct Tagger(BTreeMap<&'static str, Tag>);
@@ -60,5 +60,45 @@ async fn smoke() -> anyhow::Result<()> {
         future::ready(())
     }));
     tokio::time::sleep(Duration::from_secs(1000)).await;
+    Ok(())
+}
+
+#[tokio::test]
+async fn should_compact() -> anyhow::Result<()> {
+    let store = BanyanStore::test("compaction").await?;
+
+    // Wait for the first compaction loop to pass.
+    tokio::time::sleep(Duration::from_millis(500)).await;
+
+    let stream = store.get_or_create_own_stream(0.into())?;
+    assert!(stream.published_tree().is_none());
+    store
+        .append(
+            0.into(),
+            (0..1000).map(|_| (tags!("abc"), Payload::empty())).collect::<Vec<_>>(),
+        )
+        .await?;
+    let root_after_append = stream.published_tree().unwrap().root();
+
+    // get the events back
+    let evs = store
+        .stream_filtered_chunked(store.node_id().stream(0.into()), 0..=10000, AllQuery)
+        .take_until_signaled(tokio::time::sleep(Duration::from_secs(2)))
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .collect::<anyhow::Result<Vec<_>>>()?
+        .into_iter()
+        .flat_map(|c| c.data);
+    assert_eq!(evs.count(), 1000);
+    // Make sure the root didn't change
+    let root_after_query = stream.published_tree().unwrap().root();
+    assert_eq!(root_after_append, root_after_query);
+
+    // Wait here for compaction (or make it configurable in [`SwarmConfig`] ..)
+    tokio::time::sleep(Duration::from_secs(60)).await;
+
+    let root_after_compaction = stream.published_tree().unwrap().root();
+    assert!(root_after_append != root_after_compaction);
     Ok(())
 }
