57ef01ef9482d7719789bfedd833cf3a4fd3961d
Oliver Wangler <oliver@wngr.de>
2021-06-09 09:29:08 +0200

    assert tree pack status

--- rust/actyx/swarm/src/tests.rs
+++ rust/actyx/swarm/src/tests.rs
@@ -1,11 +1,10 @@
-use std::{collections::BTreeMap, convert::TryFrom, str::FromStr, time::Duration};
-
 use crate::BanyanStore;
 use actyx_sdk::{tags, Payload, StreamNr, Tag, TagSet};
 use ax_futures_util::{prelude::AxStreamExt, stream::interval};
 use banyan::query::AllQuery;
 use futures::{prelude::*, StreamExt};
 use libipld::Cid;
+use std::{collections::BTreeMap, convert::TryFrom, str::FromStr, time::Duration};
 
 struct Tagger(BTreeMap<&'static str, Tag>);
 
@@ -64,25 +63,31 @@ async fn smoke() -> anyhow::Result<()> {
 }
 
 #[tokio::test]
-async fn should_compact() -> anyhow::Result<()> {
+async fn should_compact_regularly() -> anyhow::Result<()> {
+    const EVENTS: usize = 10000;
     let store = BanyanStore::test("compaction").await?;
+    let start = tokio::time::Instant::now();
 
     // Wait for the first compaction loop to pass.
     tokio::time::sleep(Duration::from_millis(500)).await;
 
     let stream = store.get_or_create_own_stream(0.into())?;
     assert!(stream.published_tree().is_none());
-    store
-        .append(
-            0.into(),
-            (0..1000).map(|_| (tags!("abc"), Payload::empty())).collect::<Vec<_>>(),
-        )
-        .await?;
-    let root_after_append = stream.published_tree().unwrap().root();
+
+    // Chunk to force creation of new leaves
+    for chunk in (0..EVENTS)
+        .map(|_| (tags!("abc"), Payload::empty()))
+        .collect::<Vec<_>>()
+        .chunks(10)
+        .into_iter()
+    {
+        store.append(0.into(), chunk.to_vec()).await?;
+    }
+    let tree_after_append = stream.published_tree().unwrap();
 
     // get the events back
     let evs = store
-        .stream_filtered_chunked(store.node_id().stream(0.into()), 0..=10000, AllQuery)
+        .stream_filtered_chunked(store.node_id().stream(0.into()), 0..=u64::MAX, AllQuery)
         .take_until_signaled(tokio::time::sleep(Duration::from_secs(2)))
         .collect::<Vec<_>>()
         .await
@@ -90,15 +95,17 @@ async fn should_compact() -> anyhow::Result<()> {
         .collect::<anyhow::Result<Vec<_>>>()?
         .into_iter()
         .flat_map(|c| c.data);
-    assert_eq!(evs.count(), 1000);
+    assert_eq!(evs.count(), EVENTS);
     // Make sure the root didn't change
-    let root_after_query = stream.published_tree().unwrap().root();
-    assert_eq!(root_after_append, root_after_query);
+    let tree_after_query = stream.published_tree().unwrap();
+    assert_eq!(tree_after_append.root(), tree_after_query.root());
+    assert!(!store.data.forest.is_packed(&tree_after_query.tree())?);
 
     // Wait here for compaction (or make it configurable in [`SwarmConfig`] ..)
-    tokio::time::sleep(Duration::from_secs(60)).await;
+    tokio::time::sleep_until(start + Duration::from_secs(61)).await;
 
-    let root_after_compaction = stream.published_tree().unwrap().root();
-    assert!(root_after_append != root_after_compaction);
+    let tree_after_compaction = stream.published_tree().unwrap();
+    assert!(tree_after_append.root() != tree_after_compaction.root());
+    assert!(store.data.forest.is_packed(&tree_after_compaction.tree())?);
     Ok(())
 }
