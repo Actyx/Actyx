f690fc5c5d551783ec5ba282f937bbb19c7b5cf5
Oliver Wangler <oliver@wngr.de>
2021-06-09 20:57:25 +0200

    Add test to hammer compaction (#6864)
    * add stress test for compaction
    
    * pack tests: use tree_stream

--- rust/actyx/swarm/src/tests.rs
+++ rust/actyx/swarm/src/tests.rs
@@ -1,10 +1,14 @@
 use crate::{AxTreeExt, BanyanStore, MAX_TREE_LEVEL};
 use actyx_sdk::{tags, Offset, Payload, StreamNr, Tag, TagSet};
-use ax_futures_util::{prelude::AxStreamExt, stream::interval};
+use ax_futures_util::{
+    prelude::AxStreamExt,
+    stream::{interval, Drainer},
+};
 use banyan::query::AllQuery;
 use futures::{prelude::*, StreamExt};
 use libipld::Cid;
 use std::{collections::BTreeMap, convert::TryFrom, str::FromStr, time::Duration};
+use trees::query::TagsQuery;
 
 struct Tagger(BTreeMap<&'static str, Tag>);
 
@@ -62,6 +66,11 @@ async fn smoke() -> anyhow::Result<()> {
     Ok(())
 }
 
+fn last_item<T: Clone>(drainer: &mut Drainer<T>) -> anyhow::Result<T> {
+    let mut vec = drainer.next().ok_or_else(|| anyhow::anyhow!("Stream ended"))?;
+    vec.pop().ok_or_else(|| anyhow::anyhow!("Stream returned pending"))
+}
+
 #[tokio::test]
 async fn should_compact_regularly() -> anyhow::Result<()> {
     const EVENTS: usize = 10000;
@@ -71,8 +80,9 @@ async fn should_compact_regularly() -> anyhow::Result<()> {
     tokio::time::sleep(Duration::from_millis(500)).await;
     tokio::time::pause();
 
-    let stream = store.get_or_create_own_stream(0.into())?;
-    assert!(stream.published_tree().is_none());
+    let tree_stream = store.get_or_create_own_stream(0.into())?.tree_stream();
+    let mut tree_stream = Drainer::new(tree_stream);
+    assert_eq!(last_item(&mut tree_stream)?.count(), 0);
 
     // Chunk to force creation of new branches
     for chunk in (0..EVENTS)
@@ -83,7 +93,8 @@ async fn should_compact_regularly() -> anyhow::Result<()> {
     {
         store.append(0.into(), chunk.to_vec()).await?;
     }
-    let tree_after_append = stream.published_tree().unwrap();
+    let tree_after_append = last_item(&mut tree_stream)?;
+    assert!(!store.data.forest.is_packed(&tree_after_append)?);
 
     // get the events back
     let evs = store
@@ -97,18 +108,14 @@ async fn should_compact_regularly() -> anyhow::Result<()> {
         .flat_map(|c| c.data);
     assert_eq!(evs.count(), EVENTS);
     // Make sure the root didn't change
-    let tree_after_query = stream.published_tree().unwrap();
-    assert_eq!(tree_after_append.root(), tree_after_query.root());
-    assert!(!store.data.forest.is_packed(&tree_after_query.tree())?);
+    assert!(tree_stream.next().unwrap().is_empty());
 
     tokio::time::advance(Duration::from_secs(60)).await;
     tokio::time::resume();
-    // Wait here for compaction
-    tokio::time::sleep(Duration::from_secs(1)).await;
 
-    let tree_after_compaction = stream.published_tree().unwrap();
+    let tree_after_compaction = last_item(&mut tree_stream)?;
     assert!(tree_after_append.root() != tree_after_compaction.root());
-    assert!(store.data.forest.is_packed(&tree_after_compaction.tree())?);
+    assert!(store.data.forest.is_packed(&tree_after_compaction)?);
     Ok(())
 }
 
@@ -119,14 +126,15 @@ async fn should_extend_packed_when_hitting_max_tree_depth() -> anyhow::Result<()
     // Wait for the first compaction loop to pass.
     tokio::time::sleep(Duration::from_millis(500)).await;
 
-    let stream = store.get_or_create_own_stream(0.into())?;
-    assert!(stream.published_tree().is_none());
+    let tree_stream = store.get_or_create_own_stream(0.into())?.tree_stream();
+    let mut tree_stream = Drainer::new(tree_stream);
+    assert_eq!(last_item(&mut tree_stream)?.count(), 0);
 
     // Append individually to force creation of new branches
     for ev in (0..MAX_TREE_LEVEL + 1).map(|_| (tags!("abc"), Payload::empty())) {
         store.append(0.into(), vec![ev]).await?;
     }
-    let tree_after_append = stream.published_tree().unwrap().tree();
+    let tree_after_append = last_item(&mut tree_stream)?;
     assert!(!store.data.forest.is_packed(&tree_after_append)?);
     assert_eq!(tree_after_append.level(), MAX_TREE_LEVEL + 1);
     assert_eq!(
@@ -136,7 +144,7 @@ async fn should_extend_packed_when_hitting_max_tree_depth() -> anyhow::Result<()
 
     // packing will be triggered when the existing tree's level is MAX_TREE_LEVEL + 1
     store.append(0.into(), vec![(tags!("abc"), Payload::empty())]).await?;
-    let tree_after_pack = stream.published_tree().unwrap().tree();
+    let tree_after_pack = last_item(&mut tree_stream)?;
     // the tree is not packed
     assert!(!store.data.forest.is_packed(&tree_after_pack)?);
     // but the max level remains constant now
@@ -145,5 +153,40 @@ async fn should_extend_packed_when_hitting_max_tree_depth() -> anyhow::Result<()
         tree_after_pack.offset(),
         Some(Offset::try_from(MAX_TREE_LEVEL as i64 + 1).unwrap())
     );
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+async fn must_not_lose_events_through_compaction() -> anyhow::Result<()> {
+    let tags_query = TagsQuery::from_expr(&"'abc'".parse().unwrap())(true);
+    const EVENTS: usize = 1000;
+    let store = BanyanStore::test("compaction_max_tree").await?;
+    // compact continuously
+    store.spawn_task("compaction", store.clone().compaction_loop(Duration::from_micros(0)));
+
+    let stream = store.get_or_create_own_stream(0.into())?;
+    assert!(stream.published_tree().is_none());
+
+    for ev in (0..EVENTS).map(|_| (tags!("abc"), Payload::empty())) {
+        store.append(0.into(), vec![ev]).await?;
+    }
+
+    let evs = store
+        .stream_filtered_stream_ordered(tags_query)
+        .take(EVENTS)
+        .take_until_signaled(tokio::time::sleep(Duration::from_secs(2)))
+        .map_ok(|x| x.0)
+        .collect::<Vec<_>>()
+        .await
+        .into_iter()
+        .collect::<anyhow::Result<Vec<_>>>()?;
+    anyhow::ensure!(
+        evs.len() == EVENTS,
+        "Expected {} events, but only got back {}. Received: {:?}",
+        EVENTS,
+        evs.len(),
+        evs
+    );
+
     Ok(())
 }
