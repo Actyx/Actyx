be30c721546e57c88e746579aaaf967368764175
David Craven <david@craven.ch>
2021-06-17 12:09:20 +0200

    Support adding binary blobs. (#6942)
    * Support adding binary blobs.
    
    * Address review comments.
    
    * Fix gateway.
    
    * Update rust/actyx/swarm/src/lib.rs
    
    Co-authored-by: Oliver Wangler <oliver@wngr.de>
    
    * Update rust/actyx/swarm/src/lib.rs
    
    Co-authored-by: Oliver Wangler <oliver@wngr.de>
    
    * Update rust/actyx/swarm/src/lib.rs
    
    * Use traverse instead of resolve.
    
    Co-authored-by: Oliver Wangler <oliver@wngr.de>

--- rust/actyx/api/src/ipfs_file_gateway.rs
+++ rust/actyx/api/src/ipfs_file_gateway.rs
@@ -1,4 +1,4 @@
-use std::{collections::VecDeque, path::Path, pin::Pin, str::FromStr};
+use std::{collections::VecDeque, path::Path, str::FromStr};
 
 use anyhow::Result;
 use futures::prelude::*;
@@ -9,7 +9,7 @@ use warp::{
     filters::BoxedFilter,
     http::header::{HeaderValue, CONTENT_TYPE},
     hyper::{Body, Response},
-    path, Filter, Rejection, Reply,
+    path, Filter, Reply,
 };
 
 /// an ipfs query contains a root cid and a path into it
@@ -41,18 +41,26 @@ fn content_type_from_ext(query: &IpfsQuery) -> Option<HeaderValue> {
     HeaderValue::from_str(mime).ok()
 }
 
-fn content_type_from_first_chunk(chunk: Option<&Result<Vec<u8>>>) -> Option<HeaderValue> {
-    let mime = tree_magic::from_u8(chunk?.as_ref().ok()?.as_slice());
+fn content_type_from_first_chunk(chunk: &[u8]) -> Option<HeaderValue> {
+    let mime = tree_magic::from_u8(chunk);
     debug!("detected mime type {} from content", mime);
     HeaderValue::from_str(&mime).ok()
 }
 
-async fn handle_query(store: BanyanStore, query: IpfsQuery) -> Result<Response<Body>, Rejection> {
+async fn handle_query(store: BanyanStore, query: IpfsQuery) -> Result<Response<Body>, anyhow::Error> {
     let content_header_from_ext = content_type_from_ext(&query);
-    let mut stream = store.cat(query.root, query.path).peekable();
-    let chunk = Pin::new(&mut stream).peek().await;
-    let content_header_from_content = content_type_from_first_chunk(chunk);
-    let mut resp = Response::new(Body::wrap_stream(stream));
+    let tmp = store.ipfs().create_temp_pin()?;
+    store.ipfs().temp_pin(&tmp, &query.root)?;
+    let cid = if let Some(cid) = store.traverse(&query.root, query.path).await? {
+        cid
+    } else {
+        return Err(anyhow::anyhow!("file not found"));
+    };
+    store.ipfs().sync(&cid, store.ipfs().peers()).await?;
+    let mut buf = vec![];
+    store.cat(&cid, &mut buf)?;
+    let content_header_from_content = content_type_from_first_chunk(&buf);
+    let mut resp = Response::new(Body::from(buf));
     // extension takes precedence over content
     if let Some(ct) = content_header_from_ext.or(content_header_from_content) {
         resp.headers_mut().insert(CONTENT_TYPE, ct);
@@ -69,6 +77,6 @@ pub fn route(store: BanyanStore) -> BoxedFilter<(impl Reply,)> {
                     .map_err(|_| warp::reject::not_found()),
             )
         })
-        .and_then(move |query: IpfsQuery| handle_query(store.clone(), query))
+        .and_then(move |query: IpfsQuery| handle_query(store.clone(), query).map_err(crate::util::reject))
         .boxed()
 }
--- rust/actyx/swarm/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    prost_build::compile_protos(&["src/unixfsv1/unixfs.proto"], &["src"]).unwrap();
-}
