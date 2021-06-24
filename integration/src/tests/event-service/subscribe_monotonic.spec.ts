93f2d7337811ab72e1b839c5df72bfdae152bdfb
Roland Kuhn <rk@rkuhn.info>
2021-06-24 09:03:24 +0200

    improve subscribe* specs

--- integration/src/tests/event-service/subscribe_monotonic.spec.ts
+++ integration/src/tests/event-service/subscribe_monotonic.spec.ts
@@ -3,6 +3,7 @@ import {
   SubscribeMonotonicRequest,
   SubscribeMonotonicResponse,
 } from '../../http-client'
+import { waitFor } from '../../retry'
 import { run } from '../../util'
 import { genericCommunicationTimeout, mySuite, publishRandom, testName } from './utils.support.test'
 
@@ -21,9 +22,10 @@ describe('event service', () => {
         await new Promise((resolve, reject) => {
           es.subscribeMonotonic(request, (x) => {
             data.push(x)
-          })
-            .then(resolve)
-            .catch(reject)
+            if (data.length == 2) {
+              resolve()
+            }
+          }).catch(reject)
           setTimeout(resolve, genericCommunicationTimeout)
         })
         expect(data).toMatchObject([
@@ -45,12 +47,13 @@ describe('event service', () => {
         const done = new Promise((resolve, reject) => {
           es.subscribeMonotonic(request, (x) => {
             data.push(x)
-          })
-            .then(resolve)
-            .catch(reject)
+            if (data.length == 3) {
+              resolve()
+            }
+          }).catch(reject)
           setTimeout(resolve, genericCommunicationTimeout)
         })
-        await new Promise((resolve) => setTimeout(resolve, 1000))
+        await waitFor(() => data.length >= 2, 5_000, 50)
         const pub2 = await publishRandom(es)
         await done
         expect(data).toMatchObject([
@@ -58,6 +61,10 @@ describe('event service', () => {
           { type: 'offsets', offsets: { [pub1.stream]: expect.any(Number) } },
           pub2,
         ])
+        expect(data[1].type === 'offsets' && data[1].offsets[pub1.stream]).toBeGreaterThanOrEqual(
+          pub1.offset,
+        )
+        expect(data[1].type === 'offsets' && data[1].offsets[pub1.stream]).toBeLessThan(pub2.offset)
       }))
   })
 })
