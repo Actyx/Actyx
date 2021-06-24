53337bdfb3b4edbbf096d0c55653ca4d95e0228c
Oliver Stollmann <oliver@actyx.io>
2021-06-24 12:30:38 +0200

    Add draft/prod deployment scripts (#7052)
    * Add draft/prod deployment scripts
    
    Before this:
    - Deploy to production `npm run deploy -- not-draft`
    - Deploy draft: `npm run deploy`
    
    After this:
    - Deploy to production `npm run deploy:prod`
    - Deploy draft: `npm run deploy:draft`
    
    * Better deploy UX for docs

--- web/developer.actyx.com/bin/deploy.js
+++ web/developer.actyx.com/bin/deploy.js
@@ -1,12 +1,12 @@
-const NetlifyClient = require("netlify");
-const fs = require("fs");
-const path = require("path");
+const NetlifyClient = require('netlify')
+const fs = require('fs')
+const path = require('path')
 
-const log = console.log;
+const log = console.log
 
 const logOnProgress = (ev) => {
-  log(ev.msg);
-};
+  log(ev.msg)
+}
 
 /**
  * This function deploys a directory to a Netlify site. If draft is true, it
@@ -14,18 +14,10 @@ const logOnProgress = (ev) => {
  * a production deploy. The function waits for the deploy to succeed before
  * returning.
  */
-const deployDir = async (
-  siteId,
-  path,
-  netlifyAccessToken,
-  draft,
-  deployMsg
-) => {
-  const client = new NetlifyClient(netlifyAccessToken);
+const deployDir = async (siteId, path, netlifyAccessToken, draft, deployMsg) => {
+  const client = new NetlifyClient(netlifyAccessToken)
 
-  log(
-    `Starting site deploy (siteId: ${siteId}, path: ${path}, draft: ${draft}).`
-  );
+  log(`Starting site deploy (siteId: ${siteId}, path: ${path}, draft: ${draft}).`)
   const res = await client.deploy(siteId, path, {
     draft, // draft deploy or production deploy
     message: deployMsg,
@@ -33,59 +25,57 @@ const deployDir = async (
     parallelHash: 100,
     parallelUpload: 5,
     maxRetry: 5,
-  });
-  log(`Site deploy completed; permalink: ${res.deploy.links.permalink}`);
+  })
+  log(`Site deploy completed; permalink: ${res.deploy.links.permalink}`)
   if (!res.deploy.links.permalink) {
-    throw new Error(
-      `Netlify unexpectedly did not return a permalink to the deployed site.`
-    );
+    throw new Error(`Netlify unexpectedly did not return a permalink to the deployed site.`)
   }
-  log(`{{DEPLOY_PERMALINK=${res.deploy.links.permalink}}}`);
-  return;
-};
+  log(`{{DEPLOY_PERMALINK=${res.deploy.links.permalink}}}`)
+  return
+}
 
-(async () => {
-  const SITE_ID = "fffa1022-c8a0-4238-97c4-dd80979bf887";
-  const PATH = "build";
+;(async () => {
+  const SITE_ID = 'fffa1022-c8a0-4238-97c4-dd80979bf887'
+  const PATH = 'build'
 
-  if (process.argv.length < 3) {
-    throw new Error("Usage deploy.js <deploy-name> [--not-draft]");
+  if (process.argv.length < 4) {
+    console.error('*******************************************************************************')
+    console.error('** Invalid usage of deploy script. If you are using `npm run deploy:prod` or **')
+    console.error('** `npm run deploy:draft`, add a name as shown in the following example:     **')
+    console.error('** `npm run deploy:draft -- "My draft deployment"`                           **')
+    console.error('*******************************************************************************')
+    throw new Error('Usage deploy.js <deploy-type> <deploy-name>')
   }
-  DEPLOY_NAME = process.argv[2];
-  console.log(`Deploy name: ${DEPLOY_NAME}`);
-  const NOT_DRAFT =
-    process.argv.length > 3 && process.argv[3] === "--not-draft";
+  DEPLOY_TYPE = process.argv[2]
+  DEPLOY_NAME = process.argv[3]
+  console.log(`Deploy type: ${DEPLOY_TYPE}`)
+  console.log(`Deploy name: ${DEPLOY_NAME}`)
+  if (!['draft', 'prod'].includes(DEPLOY_TYPE)) {
+    throw new Error("<deploy-type> must be one of 'draft' or 'prod'")
+  }
+  if (!DEPLOY_NAME) {
+    throw new Error('<deploy-name> is not set or empty')
+  }
 
-  const NETLIFY_ACCESS_TOKEN = process.env.NETLIFY_ACCESS_TOKEN;
+  const NETLIFY_ACCESS_TOKEN = process.env.NETLIFY_ACCESS_TOKEN
 
   if (!NETLIFY_ACCESS_TOKEN) {
-    console.error(
-      "Required environment variable NETLIFY_ACCESS_TOKEN not found"
-    );
-    process.exit(1);
+    console.error('Required environment variable NETLIFY_ACCESS_TOKEN not found')
+    process.exit(1)
   }
 
-  if (!fs.existsSync(path.join(__dirname, "..", PATH, "index.html"))) {
+  if (!fs.existsSync(path.join(__dirname, '..', PATH, 'index.html'))) {
     console.error(
-      `Unable to find file ${path.join(
-        PATH,
-        "index.html"
-      )}; are you sure the site has been built?`
-    );
-    process.exit(1);
+      `Unable to find file ${path.join(PATH, 'index.html')}; are you sure the site has been built?`,
+    )
+    process.exit(1)
   }
 
   try {
-    await deployDir(
-      SITE_ID,
-      PATH,
-      NETLIFY_ACCESS_TOKEN,
-      !NOT_DRAFT,
-      DEPLOY_NAME
-    );
+    await deployDir(SITE_ID, PATH, NETLIFY_ACCESS_TOKEN, !(DEPLOY_TYPE === 'prod'), DEPLOY_NAME)
   } catch (error) {
-    console.error("Error deploying directory");
-    console.error(error);
-    process.exit(1);
+    console.error('Error deploying directory')
+    console.error(error)
+    process.exit(1)
   }
-})();
+})()
