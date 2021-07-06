6261d547d4d4dbb05b996a5fa28d66cb1d3b9168
André Rüdiger <andre.ruediger@gmail.com>
2021-07-06 12:36:10 +0200

    make Program3 integration test terminate

--- dotnet/Actyx-SDK/Sdk.IntegrationTests/Program3.cs
+++ dotnet/Actyx-SDK/Sdk.IntegrationTests/Program3.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Threading;
 using System.Threading.Tasks;
 using Actyx;
 using Actyx.Sdk.AxHttpClient;
 using Actyx.Sdk.AxWebsocketClient;
 using Actyx.Sdk.Formats;
+using DeepEqual.Syntax;
 
 namespace Sdk.IntegrationTests
 {
     class Program3
     {
+        const int N = 100;
+
         static async Task Main()
         {
-            var exitEvent = new ManualResetEvent(false);
-
             var manifest = new AppManifest()
             {
                 AppId = "com.example.ax-ws-client-tests",
@@ -29,31 +31,24 @@ namespace Sdk.IntegrationTests
             var wsrpcClient = new WsrpcClient(new Uri($"ws://localhost:4454/api/v2/events?{token}"));
             using var store = new WebsocketEventStore(wsrpcClient, "com.example.ax-ws-client-tests", nodeId);
 
-            var tag = RandomString(8);
+            List<string> tags = new() { RandomString(8) };
 
-            var _ = Task.Run(async () =>
+            var persisted =
+                Observable
+                    .Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(50))
+                    .SelectMany(_ =>
+                        store.Publish(new List<IEventDraft>() {
+                            new EventDraft { Tags = tags, Payload = "event" },
+                        }).ToObservable().SelectMany(x => x)
+                    );
+            var subscribe =
+                store
+                    .Subscribe(null, new Aql($"FROM '{tags[0]}'"))
+                    .OfType<EventOnWire>();
+            await foreach (var (p, s) in persisted.Zip(subscribe).Take(N).ToAsyncEnumerable())
             {
-                var persist = Observable
-                    .Timer(TimeSpan.Zero, TimeSpan.FromSeconds(5))
-                    .SelectMany(x => Observable.FromAsync(() => store.Publish(new List<IEventDraft>() {
-                        new EventDraft { Tags = new List<string>() { tag }, Payload = "live_event" },
-                    })));
-                await foreach (var batch in persist.ToAsyncEnumerable())
-                {
-                    foreach (var p in batch)
-                    {
-                        Console.WriteLine($"persisted: {Proto<EventOnWire>.Serialize(p)}");
-                    }
-                }
-            });
-
-            var subscribe = store.Subscribe(null, new Aql($"FROM '{tag}'"));
-            await foreach (var s in subscribe.ToAsyncEnumerable())
-            {
-                Console.WriteLine($"subscribed: {Proto<IEventOnWire>.Serialize(s)}");
+                p.ShouldDeepEqual(s);
             }
-
-            exitEvent.WaitOne();
         }
 
         private static readonly Random random = new();
