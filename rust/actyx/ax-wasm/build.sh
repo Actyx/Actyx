c002e6b7b41b4cbf8fd0aca0969e69662b7acb7a
Oliver Wangler <oliver@wngr.de>
2021-08-24 09:48:57 +0200

    add custom build script in favor of wasm-pack

---
+++ rust/actyx/ax-wasm/build.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -e
+WASMOPT=./wasm-opt
+WASMOPT_VERSION=101
+WASMBINDGEN_VERSION=0.2.76
+OUT=./pkg
+
+if [ -d $OUT ]; then
+  echo "Clearing output directory '$OUT'"
+  rm -rf $OUT
+fi
+
+if ! [ -x "$(command -v wasm-bindgen)" ]; then
+  echo "Installing wasm-bindgen-cli via cargo"
+  cargo install wasm-bindgen-cli --version 0.2.76
+fi
+
+echo "Generating wasm-bindings"
+
+# add supports for Weak References, see [1].
+# TLDR: Structs passed from Rust to JS will be deallocated
+# automatically, no need to call `.free` in JS.
+#
+# [1]: https://rustwasm.github.io/docs/wasm-bindgen/reference/weak-references.html
+wasm-bindgen ../target/wasm32-unknown-unknown/release/ax_wasm.wasm \
+  --out-dir $OUT \
+  --target web \
+  --typescript \
+  --weak-refs
+
+echo "Generating package.json"
+cat <<EOF >> $OUT/package.json
+{
+  "name": "ax-wasm",
+  "version": "0.1.0",
+  "files": [
+    "ax_wasm_bg.wasm",
+    "ax_wasm.js",
+    "ax_wasm.d.ts"
+  ],
+  "module": "ax_wasm.js",
+  "types": "ax_wasm.d.ts",
+  "sideEffects": false
+}
+EOF
+
+
+if [ ! -f $WASMOPT ]; then
+  echo "Downloading wasm-opt"
+  wget -qO- \
+  https://github.com/WebAssembly/binaryen/releases/download/version_$WASMOPT_VERSION/binaryen-version_$WASMOPT_VERSION-x86_64-linux.tar.gz \
+  | tar xfz - binaryen-version_$WASMOPT_VERSION/bin/wasm-opt -O >> $WASMOPT
+  chmod +x $WASMOPT
+fi
+
+echo "Optimizing wasm bindings with default optimization (this might take some time)"
+./wasm-opt $OUT/ax_wasm_bg.wasm -O -g --output $OUT/ax_wasm_bg.opt.wasm
+
+echo "Find your wasm package in $OUT"
