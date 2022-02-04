d946b32cdace404500172c32a76eec628ee1870c
Roland Kuhn <rk@rkuhn.info>
2022-02-04 10:48:16 +0100

    fix build-monorepo-dependencies, add changelog
    feat(actyx): ability to output logs as JSON
    feat(actyx): always run in foreground on Windows
    feat(actyx): no more tray icon on Windows

--- web/developer.actyx.com/bin/build-monorepo-dependencies.js
+++ web/developer.actyx.com/bin/build-monorepo-dependencies.js
@@ -1,57 +1,39 @@
-const execa = require('execa')
-const path = require('path')
-const fs = require('fs')
+;(async () => {
+  const { execa } = await import('execa')
+  const path = require('path')
 
-const npmInstallAndBuild = async (cwd) => {
-  if (!fs.existsSync(path.join(cwd, 'node_modules'))) {
+  const npmInstallAndBuild = async (cwd) => {
     console.log(`npm:[${cwd}] installing dependencies ...`)
     try {
-      await execa('npm', ['install'], {
-        cwd,
-        shell: true,
-      })
+      await execa('npm', ['install'], { cwd })
     } catch (error) {
       console.log(`npm:[${cwd}] error installing dependencies (errors below)`)
       console.log(error)
-      return
+      throw new Error('aborting build')
     }
-  } else {
-    console.log(`npm:[${cwd}] dependencies already installed (found node_modules)`)
-  }
-  if (!fs.existsSync(path.join(cwd, 'lib'))) {
     console.log(`npm:[${cwd}] building ...`)
     try {
-      await execa('npm', ['run', 'build'], {
-        cwd,
-        shell: true,
-      })
+      await execa('npm', ['run', 'build'], { cwd })
     } catch (error) {
       console.log(`npm:[${cwd}] error building package (see errors below)`)
       console.log(error)
-      return
+      throw new Error('aborting build')
     }
-  } else {
-    console.log(`npm:[${cwd}] package already built (found lib)`)
+    console.log(`npm:[${cwd}] done!`)
   }
-  console.log(`npm:[${cwd}] done!`)
-}
 
-const cargoBuild = async (cwd) => {
-  console.log(`cargo:[${cwd}] build`)
-  try {
-    await execa('cargo', ['build', '--release'], {
-      cwd,
-      shell: true,
-    })
-  } catch (error) {
-    console.log(`cargo:[${cwd}] error build (errors below)`)
-    console.log(error)
-    return
+  const cargoBuild = async (cwd) => {
+    console.log(`cargo:[${cwd}] build`)
+    try {
+      await execa('cargo', ['build', '--release'], { cwd })
+    } catch (error) {
+      console.log(`cargo:[${cwd}] error build (errors below)`)
+      console.log(error)
+      throw new Error('aborting build')
+    }
+    console.log(`cargo:[${cwd}] done`)
   }
-  console.log(`cargo:[${cwd}] done`)
-}
 
-;(async () => {
   const npmDeps = [
     ['..', '..', 'js', 'sdk'],
     ['..', '..', 'js', 'pond'],
@@ -61,5 +43,5 @@ const cargoBuild = async (cwd) => {
     []
       .concat(npmDeps.map((d) => npmInstallAndBuild(path.join(...d))))
       .concat(cargoDeps.map((d) => cargoBuild(path.join(...d)))),
-  )
+  ).catch(() => process.exit(1))
 })()
