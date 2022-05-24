a1478202e15c2203af94ebbee1530906b0952191
Roland Kuhn <rk@rkuhn.info>
2022-05-24 12:45:50 +0200

    also set pipeline vars in integration/set_vars.sh

--- integration/set_vars.sh
+++ integration/set_vars.sh
@@ -2,3 +2,6 @@
 export AUTH_TEST_LICENSE=`vault kv get -field=test_license secret/sec.actyx/signing/actyx`
 export AUTH_TEST_NODE_LICENSE=`vault kv get -field=test_node_license secret/sec.actyx/signing/actyx`
 export AUTH_TEST_SIGNATURE=`vault kv get -field=test_signature secret/sec.actyx/signing/actyx`
+export ACTYX_PUBLIC_KEY=`vault kv get -field=public secret/sec.actyx/signing/actyx`
+export ACTYX_VERSION=`cargo run --manifest-path ../rust/release/Cargo.toml -q get-actyx-version actyx`
+export ACTYX_VERSION_CLI=`cargo run --manifest-path ../rust/release/Cargo.toml -q get-actyx-version cli`
