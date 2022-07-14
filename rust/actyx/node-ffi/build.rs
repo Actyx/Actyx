fcf10425673878c2160ae584f9aaf4c5bef4af26
Roland Kuhn <rk@rkuhn.info>
2022-07-14 17:32:01 +0200

    port Android6 fix to Rust 1.61

--- rust/actyx/node-ffi/build.rs
+++ rust/actyx/node-ffi/build.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("cargo:rerun-if-env-changed=ANDROID6");
     if std::env::var("ANDROID6").is_ok() {
         cc::Build::new()
+            .cargo_metadata(false)
             .include("android_headers")
             .file("ifaddrs.c")
             .compile("ifaddrs");
+        println!("cargo:rustc-link-lib=static:+whole-archive=ifaddrs");
+        println!(
+            "cargo:rustc-link-search=native={}",
+            std::env::var("OUT_DIR").expect("cannot build in non-UTF8 target directory")
+        );
     }
 }
