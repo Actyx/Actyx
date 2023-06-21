c72ec2e672e178a0c410f0e856f2caa52f9607f6
José Duarte <duarte.gmj@gmail.com>
2023-06-21 16:28:15 +0100

    Docker credentials pt 2

--- integration/src/infrastructure/linux.ts
+++ integration/src/infrastructure/linux.ts
@@ -74,15 +74,15 @@ export const mkNodeSshDocker = async (
     await awaitCloudInitSetup(ssh)
   }
   await ensureDocker(ssh, nodeName, target.arch)
-  const userPass = await execa('vault', [
-    'kv',
-    'get',
-    '--format=json',
-    'secret/ops.actyx.dockerhub.deployUser',
-  ])
-  if (userPass.exitCode !== 0) {
-    throw new Error('cannot get dockerhub credentials - you need to be authenticated to vault')
-  }
+  // const userPass = await execa('vault', [
+  //   'kv',
+  //   'get',
+  //   '--format=json',
+  //   'secret/ops.actyx.dockerhub.deployUser',
+  // ])
+  // if (userPass.exitCode !== 0) {
+  //   throw new Error('cannot get dockerhub credentials - you need to be authenticated to vault')
+  // }
   const user = process.env["DOCKER_USERNAME"]
   const pass = process.env["DOCKER_PASSWORD"]
   //const { user, pass } = JSON.parse(userPass.stdout).data.data
