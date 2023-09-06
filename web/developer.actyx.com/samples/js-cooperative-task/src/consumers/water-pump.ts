54a607fd3f0e683c42a3b88cc234a66d9612eb0d
Alan Darmasaputra <kelerchian@gmail.com>
2023-09-06 13:28:15 +0200

    add local task placeholder in the sample code

--- web/developer.actyx.com/samples/js-cooperative-task/src/consumers/water-pump.ts
+++ web/developer.actyx.com/samples/js-cooperative-task/src/consumers/water-pump.ts
@@ -3,21 +3,27 @@ import { WaterPump, protocol } from "../machines";
 import { Actyx } from "@actyx/sdk";
 
 export const supplyWater = async (actyx: Actyx, dockingId: string) => {
-  console.log("pump starts task:", dockingId)
+  console.log("pump starts task:", dockingId);
 
   const tag = protocol.protocol.tagWithEntityId(dockingId);
   const machine = createMachineRunner(actyx, tag, WaterPump.ClearingDock, undefined);
 
   for await (const state of machine) {
-    console.log("pump is:", state.type)
+    console.log("pump is:", state.type);
 
     const whenInitial = state.as(WaterPump.ClearingDock);
     if (whenInitial) {
+      // Here the actual work would happen
+      // Sleeping for a second as the customary replacement
+      await sleep(1000);
       await whenInitial.commands?.dockAvailable();
     }
 
     const whenPumping = state.as(WaterPump.PumpingWater);
     if (whenPumping) {
+      // Here the actual work would happen
+      // Sleeping for a second as the customary replacement
+      await sleep(1000);
       await whenPumping.commands?.waterSupplied();
     }
 
@@ -27,5 +33,7 @@ export const supplyWater = async (actyx: Actyx, dockingId: string) => {
     }
   }
 
-  console.log("pump finishes task:", dockingId)
+  console.log("pump finishes task:", dockingId);
 };
+
+const sleep = (dur: number) => new Promise((res) => setTimeout(res, dur));
--- web/developer.actyx.com/samples/js-cooperative-task/src/consumers/watering-robot.ts
+++ web/developer.actyx.com/samples/js-cooperative-task/src/consumers/watering-robot.ts
@@ -3,26 +3,27 @@ import { WateringRobot, protocol } from "../machines";
 import { Actyx } from "@actyx/sdk";
 
 export const dockAndDrawWater = async (actyx: Actyx, dockingId: string) => {
-  console.log("robot starts task:", dockingId)
+  console.log("robot starts task:", dockingId);
 
   const tag = protocol.protocol.tagWithEntityId(dockingId);
-  const machine = createMachineRunner(
-    actyx,
-    tag,
-    WateringRobot.WaitingForAvailableDock,
-    undefined
-  );
+  const machine = createMachineRunner(actyx, tag, WateringRobot.WaitingForAvailableDock, undefined);
 
   for await (const state of machine) {
-    console.log("robot is:", state.type)
+    console.log("robot is:", state.type);
 
     const whenDocking = state.as(WateringRobot.Docking);
     if (whenDocking) {
+      // Here the actual work would happen
+      // Sleeping for a second as the customary replacement
+      await sleep(1000);
       await whenDocking.commands?.docked();
     }
 
     const whenWaterPumped = state.as(WateringRobot.Undocking);
     if (whenWaterPumped) {
+      // Here the actual work would happen
+      // Sleeping for a second as the customary replacement
+      await sleep(1000);
       await whenWaterPumped.commands?.undocked();
     }
 
@@ -32,5 +33,7 @@ export const dockAndDrawWater = async (actyx: Actyx, dockingId: string) => {
     }
   }
 
-  console.log("robot finishes task:", dockingId)
+  console.log("robot finishes task:", dockingId);
 };
+
+const sleep = (dur: number) => new Promise((res) => setTimeout(res, dur));
