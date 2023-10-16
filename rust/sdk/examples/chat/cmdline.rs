182479f642840ae5bc6f418800616e55704d0e16
Alan Darmasaputra <kelerchian@gmail.com>
2023-10-16 12:58:36 +0200

    fix double-input issue in windows and add identity changing mechanism

--- rust/sdk/examples/chat/cmdline.rs
+++ rust/sdk/examples/chat/cmdline.rs
@@ -3,7 +3,7 @@ use crate::{
     messages::{self, Messages},
 };
 use acto::{variable::Writer, ActoCell, ActoInput, ActoRef, ActoRuntime};
-use crossterm::event::{Event, KeyCode};
+use crossterm::event::{Event, KeyCode, KeyEventKind, KeyModifiers};
 use ratatui::widgets::{Block, Borders};
 use tui_textarea::TextArea;
 
@@ -12,14 +12,40 @@ pub enum Cmdline {
     Reconnect(ActoRef<Messages>),
 }
 
-fn mk_text_area() -> TextArea<'static> {
+fn mk_message_text_area() -> TextArea<'static> {
     let mut text_area = TextArea::default();
     text_area.set_block(
         Block::default()
-            .title("your message (Enter to send, Ctrl-C to quit)")
+            .title("your message (Enter to send, Ctrl-I to Edit Identity, Ctrl-C to quit)")
             .borders(Borders::ALL),
     );
     text_area
+}
+
+fn mk_identity_text_area(s: &String) -> TextArea<'static> {
+    let mut text_area: TextArea<'_> = TextArea::default();
+    text_area.set_block(
+        Block::default()
+            .title("your identity (Enter to apply, Escape to cancel or use default, Ctrl-C to quit)")
+            .borders(Borders::ALL),
+    );
+    text_area.insert_str(s);
+    text_area
+}
+
+pub struct Identity {
+    pub edit: Option<TextArea<'static>>,
+    pub val: TextArea<'static>,
+}
+
+impl Default for Identity {
+    fn default() -> Self {
+        let me: String = "me".into();
+        Self {
+            edit: Some(mk_identity_text_area(&me)),
+            val: mk_identity_text_area(&me),
+        }
+    }
 }
 
 pub async fn cmdline(
@@ -27,25 +53,74 @@ pub async fn cmdline(
     display: ActoRef<Display>,
     mut messages: ActoRef<Messages>,
 ) {
-    let text_area = Writer::new(mk_text_area());
+    let identity = Writer::new(Identity::default());
+    let text_area = Writer::new(mk_message_text_area());
+    display.send(Display::UpdateIdentity(identity.reader()));
     display.send(Display::Cmdline(text_area.reader()));
 
     while let ActoInput::Message(msg) = cell.recv().await {
         match msg {
             Cmdline::Event(event) => {
-                if let Event::Key(key) = event {
-                    // FIXME modifiers are not recognised, so no ctrl-enter
-                    if key.code == KeyCode::Enter {
-                        let mut text_area = text_area.write();
-                        let text = std::mem::replace(&mut *text_area, mk_text_area())
-                            .into_lines()
-                            .join("\n");
-                        tracing::info!("publishing message: {}", text);
-                        messages.send(Messages::Publish(messages::Event::new("me".to_owned(), text)));
-                    } else {
-                        text_area.write().input(key);
+                // Prevent double input because of KeyEventKind::Release
+                let key = match event {
+                    Event::Key(key) if key.kind == KeyEventKind::Press || key.kind == KeyEventKind::Repeat => key,
+                    _ => continue,
+                };
+
+                let is_editing_identity = { (identity.read()).edit.clone() };
+                match is_editing_identity {
+                    // When editing identity
+                    Some(identity_buffer_value) => {
+                        {
+                            let mut identity = identity.write();
+                            match key.code {
+                                KeyCode::Esc => {
+                                    identity.edit = None;
+                                }
+                                KeyCode::Enter => {
+                                    identity.val = identity_buffer_value;
+                                    identity.edit = None;
+                                }
+                                _ => {
+                                    if let Some(edit) = &mut identity.edit {
+                                        edit.input(key);
+                                    }
+                                }
+                            }
+                        }
+                        display.send(Display::UpdateIdentity(identity.reader()));
                     }
-                    display.send(Display::Cmdline(text_area.reader()));
+                    // When chatting
+                    None => {
+                        match key.code {
+                            KeyCode::Char('i') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                                {
+                                    let mut identity = identity.write();
+                                    identity.edit = Some(identity.val.clone());
+                                }
+                                display.send(Display::UpdateIdentity(identity.reader()));
+                            }
+                            _ => {
+                                // FIXME modifiers are not recognised, so no ctrl-enter
+                                if key.code == KeyCode::Enter {
+                                    let mut text_area = text_area.write();
+                                    let text = std::mem::replace(&mut *text_area, mk_message_text_area())
+                                        .into_lines()
+                                        .join("\n");
+                                    tracing::info!("publishing message: {}", text);
+                                    let name = {
+                                        let val = &identity.read().val;
+                                        let lines = val.clone().into_lines().join("\n");
+                                        lines
+                                    };
+                                    messages.send(Messages::Publish(messages::Event::new(name, text)));
+                                } else {
+                                    text_area.write().input(key);
+                                }
+                                display.send(Display::Cmdline(text_area.reader()));
+                            }
+                        }
+                    }
                 }
             }
             Cmdline::Reconnect(m) => messages = m,
--- rust/sdk/examples/chat/display.rs
+++ rust/sdk/examples/chat/display.rs
@@ -1,4 +1,4 @@
-use crate::messages::Message;
+use crate::{cmdline::Identity, messages::Message};
 use acto::{
     variable::{Reader, Writer},
     ActoCell, ActoInput, ActoRuntime,
@@ -9,7 +9,7 @@ use ratatui::{
     style::{Color, Modifier, Style},
     text::{Line, Span},
     widgets::{Block, Borders, Clear, Paragraph, Wrap},
-    Terminal,
+    Frame, Terminal,
 };
 use std::io;
 use tui_textarea::TextArea;
@@ -52,6 +52,7 @@ pub enum Display {
     Cmdline(Reader<TextArea<'static>>),
     Messages(Reader<Vec<Message>>),
     NotConnected(String),
+    UpdateIdentity(Reader<Identity>),
     Connected,
 }
 
@@ -59,6 +60,7 @@ pub async fn display(mut cell: ActoCell<Display, impl ActoRuntime>) {
     let _guard = guard::TermGuard::new();
     let mut terminal = Terminal::new(CrosstermBackend::new(io::stdout())).expect("failed to create terminal");
 
+    let mut identity = Writer::new(Identity::default()).reader();
     let mut text_area = Writer::new(TextArea::default()).reader();
     let mut messages = Writer::new(Vec::new()).reader();
     let mut not_connected = None;
@@ -69,62 +71,96 @@ pub async fn display(mut cell: ActoCell<Display, impl ActoRuntime>) {
             Display::Messages(m) => messages = m,
             Display::NotConnected(e) => not_connected = Some(e),
             Display::Connected => not_connected = None,
+            Display::UpdateIdentity(i) => identity = i,
         }
         let res = terminal.draw(|f| {
-            let size = f.size();
-            let cmdheight = text_area.project(|t| t.lines().len() as u16);
-            let layout = Layout::default()
-                .direction(ratatui::prelude::Direction::Vertical)
-                .constraints([
-                    ratatui::prelude::Constraint::Min(0),
-                    ratatui::prelude::Constraint::Length(cmdheight + 2),
-                ])
-                .split(size);
-
-            messages.project(|msgs| {
-                let mut lines = vec![];
-                for msg in msgs {
-                    let time = DateTime::<Utc>::from(msg.time).to_rfc3339();
-                    lines.push(Line::from(vec![
-                        Span::styled(time, Style::new().add_modifier(Modifier::ITALIC)),
-                        Span::raw(" "),
-                        Span::styled(&msg.from, Style::new().add_modifier(Modifier::BOLD)),
-                        Span::raw(": "),
-                        Span::raw(&msg.text),
-                    ]));
-                }
-                f.render_widget(Paragraph::new(lines), layout[0]);
-            });
-
-            text_area.project(|t| f.render_widget(t.widget(), layout[1]));
-
-            if let Some(ref e) = not_connected {
-                let rect = centered_rect(60, 20, size);
-                let text = Paragraph::new(vec![
-                    Line::from(Span::styled(
-                        "Not connected to Actyx",
-                        Style::new().add_modifier(Modifier::BOLD),
-                    )),
-                    Line::from(""),
-                    Line::from(e.as_str()),
-                ])
-                .alignment(Alignment::Center)
-                .wrap(Wrap { trim: true })
-                .block(
-                    Block::default()
-                        .title("Error")
-                        .borders(Borders::ALL)
-                        .style(Style::new().bg(Color::Red)),
-                );
-                f.render_widget(Clear, rect);
-                f.render_widget(text, rect);
-            }
+            render(f, &identity, &text_area, &messages, &not_connected);
         });
         if let Err(e) = res {
             tracing::error!("failed to draw terminal: {}", e);
             return;
         }
     }
+}
+
+fn render<W: io::Write>(
+    f: &mut Frame<CrosstermBackend<W>>,
+    identity: &Reader<Identity>,
+    text_area: &Reader<TextArea>,
+    messages: &Reader<Vec<Message>>,
+    not_connected: &Option<String>,
+) {
+    identity.project(|identity| match &identity.edit {
+        Some(edit) => render_editing_identity(f, edit),
+        None => render_chat(f, text_area, messages, not_connected),
+    });
+}
+
+fn render_editing_identity<W: io::Write>(f: &mut Frame<CrosstermBackend<W>>, edit: &TextArea) {
+    let size = f.size();
+    let layout = Layout::default()
+        .direction(ratatui::prelude::Direction::Vertical)
+        .constraints([ratatui::prelude::Constraint::Length(1)])
+        .split(size);
+
+    f.render_widget(edit.widget(), layout[0]);
+}
+
+fn render_chat<W: io::Write>(
+    f: &mut Frame<CrosstermBackend<W>>,
+    text_area: &Reader<TextArea>,
+    messages: &Reader<Vec<Message>>,
+    not_connected: &Option<String>,
+) {
+    let size = f.size();
+    let cmdheight = text_area.project(|t| t.lines().len() as u16);
+    let layout = Layout::default()
+        .direction(ratatui::prelude::Direction::Vertical)
+        .constraints([
+            ratatui::prelude::Constraint::Min(0),
+            ratatui::prelude::Constraint::Length(cmdheight + 2),
+        ])
+        .split(size);
+
+    messages.project(|msgs| {
+        let lines = msgs.into_iter().map(message_to_line).collect::<Vec<_>>();
+        f.render_widget(Paragraph::new(lines), layout[0]);
+    });
+
+    text_area.project(|t| f.render_widget(t.widget(), layout[1]));
+
+    if let Some(ref e) = not_connected {
+        let rect = centered_rect(60, 20, size);
+        let text = Paragraph::new(vec![
+            Line::from(Span::styled(
+                "Not connected to Actyx",
+                Style::new().add_modifier(Modifier::BOLD),
+            )),
+            Line::from(""),
+            Line::from(e.as_str()),
+        ])
+        .alignment(Alignment::Center)
+        .wrap(Wrap { trim: true })
+        .block(
+            Block::default()
+                .title("Error")
+                .borders(Borders::ALL)
+                .style(Style::new().bg(Color::Red)),
+        );
+        f.render_widget(Clear, rect);
+        f.render_widget(text, rect);
+    }
+}
+
+fn message_to_line(msg: &Message) -> Line {
+    let time = DateTime::<Utc>::from(msg.time).to_rfc3339();
+    Line::from(vec![
+        Span::styled(time, Style::new().add_modifier(Modifier::ITALIC)),
+        Span::raw(" "),
+        Span::styled(&msg.from, Style::new().add_modifier(Modifier::BOLD)),
+        Span::raw(": "),
+        Span::raw(&msg.text),
+    ])
 }
 
 /// helper function to create a centered rect using up certain percentage of the available rect `r`
