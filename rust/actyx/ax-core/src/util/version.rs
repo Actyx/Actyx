a61287a01b67cb4cf6348ee0ca4bd8d709673dde
José Duarte <duarte.gmj@gmail.com>
2023-11-28 14:45:12 +0000

    Fix a detail and add another

--- rust/actyx/ax-core/src/util/version.rs
+++ rust/actyx/ax-core/src/util/version.rs
@@ -60,7 +60,7 @@ lazy_static! {
     pub static ref VERSION: String = NodeVersion::get().to_string();
 }
 
-const fn os() -> &'static str {
+const fn arch() -> &'static str {
     #[cfg(target_arch = "i686")]
     return "x86";
     #[cfg(target_arch = "x86_64")]
@@ -71,9 +71,12 @@ const fn os() -> &'static str {
     return "android";
     #[cfg(target_arch = "arm")]
     return "arm";
+    // not officially documented but let's see
+    #[cfg(target_arch = "armv7")]
+    return "armv6";
 }
 
-const fn arch() -> &'static str {
+const fn os() -> &'static str {
     #[cfg(target_os = "linux")]
     return "linux";
     #[cfg(target_os = "windows")]
